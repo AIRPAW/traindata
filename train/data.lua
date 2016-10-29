13b35c9b522edea1d259e2a44581d6594bc657b0
irina662 <voron.sp@gmail.com>
2016-10-30 05:37:44 +0700

    data load and mix

--- train/data.lua
+++ train/data.lua
@@ -1,14 +1,16 @@
 require 'torch'
 require 'image'
 
-local traintDir = "/home/ira/working/images/"
+-- local traintDir = "/home/ira/working/images/"
+local traintDir = "/home/uml/working/traindata/images/"
 local trsize = 3
 local num = 10
 local size = {x = 200, y = 30}
 local category = {"button", "checkbox", "other"}
-local channels = 3
+local channels = 1
 local img = torch.Tensor(num*trsize,channels,size.x,size.y)
 local labels = torch.Tensor(num*trsize)
+local trainPortion = 0.9
 
 for i = 1,#category do
   local index = (i-1)*num
@@ -16,6 +18,35 @@ for i = 1,#category do
   for j = 1, num do
     img[index+j] = image.load(name ..category[i] ..j..".jpg")
     labels[index+j] = i
-    print(category[i])
   end
+end
+
+local toMix = torch.randperm(labels:size())
+local trainSize = math.floor(toMix:size()*trainPortion)
+local testSize = toMix:size() - trainSize
+
+trainData = {
+  img = torch.Tensor(trainSize, channels, size.x, size.y)
+  labels = torch.Tensor(trainSize)
+  size = function()
+    return trainSize
+  end
+}
+
+testData = {
+  img = torch.Tensor(testSize, channels, size.x, size.y)
+  labels = torch.Tensor(testSize)
+  size = function()
+    return testSize
+  end
+}
+
+for i = 1, #trainSize do
+  trainData.img[i] = img[toMix[i]]:clone()
+  trainData.labels[i] = labels[toMix[i]]
+end
+
+for i = 1, #testSize do
+  trainData.img[i] = img[toMix[i + trainSize]]:clone()
+  trainData.labels[i] = labels[toMix[i + trainSize]]
 end
